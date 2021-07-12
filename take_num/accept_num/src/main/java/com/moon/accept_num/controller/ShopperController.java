da06610858f2cba373882936023fa2d6c6789168
aimoon23 <1297564418@qq.com>
2021-07-12 17:34:12 +0800

    提交代码

---
+++ take_num/accept_num/src/main/java/com/moon/accept_num/controller/ShopperController.java
@@ -0,0 +1,11 @@
+package com.moon.accept_num.controller;
+
+/**
+ * @ClassName ShopperController
+ * @Description: TODO
+ * @Author zyl
+ * @Date 2021/7/12
+ * @Version V1.0
+ **/
+public class ShopperController {
+}
