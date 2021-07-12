da25f332b9d6e5aaddb277b37bef84eee47eb67c
aimoon23 <1297564418@qq.com>
2021-07-12 17:31:55 +0800

    提交代码

--- take_num/accept_num/src/main/java/com/moon/accept_num/controller/CustomerController.java
+++ take_num/accept_num/src/main/java/com/moon/accept_num/controller/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/take/num/")
-public class TakeNumController {
+public class CustomerController {
 
 
     /**
